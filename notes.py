f453b19c61e011913e1af2e53c1c453dfc8c257c
damonbogich <damonbogich@gmail.com>
2020-07-22 16:48:54 -0400

    single number function working

---
+++ notes.py
@@ -0,0 +1,19 @@
+"""
+input: int
+output: bool
+
+Take in a positive number and decide if the number divides itself
+"""
+
+
+def divides_self(num):
+    #if num <= 0:
+        #return False
+    #elif num < 10:
+        # return True
+    
+    #split num into individual digits
+    #loop through each digit in num
+        #if digit does not evenly divides into num or if digit = 0 
+            #return False
+        #if we get through all of the digits return true
--- single_number/extra.py
+++ single_number/extra.py
@@ -1,7 +1,3 @@
-'''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
-'''
 def single_number(arr): 
     
     for i in range(0, len(arr) - 1):
@@ -21,13 +17,6 @@ def single_number(arr):
                     searching = False
                 else:
                     compare += 1
+            
 
-
-
-
-
-if __name__ == '__main__':
-    # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
-
-    print(f"The odd-number-out is {single_number(arr)}")
+single_number([1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0])
